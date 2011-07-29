d86e4d9eab33d858f5994d2da661637bf970df44
chiel <chieltbn@gmail.com>
2011-07-29 16:46:55 +0200

    Slug field for Question now autopopulates based on the Question.

--- faq/admin.py
+++ faq/admin.py
@@ -8,6 +8,7 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ['text', 'sort_order', 'created_by', 'created_on',
                     'updated_by', 'updated_on', 'status']
     list_editable = ['sort_order', 'status']
+    prepopulated_fields = { 'slug': ('text',)}
 
     def save_model(self, request, obj, form, change): 
         '''
